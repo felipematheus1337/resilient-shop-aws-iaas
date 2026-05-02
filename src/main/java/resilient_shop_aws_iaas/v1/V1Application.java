897988c95787714985966d94f26b8e6fd5ffc00d
felipematheus1337 <lipehbr@gmail.com>
2026-05-01 21:59:53 -0300

    Projeto criado.

---
+++ src/main/java/resilient_shop_aws_iaas/v1/V1Application.java
@@ -0,0 +1,13 @@
+package resilient_shop_aws_iaas.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V1Application.class, args);
+	}
+
+}
---
+++ src/test/java/resilient_shop_aws_iaas/v1/V1ApplicationTests.java
@@ -0,0 +1,13 @@
+package resilient_shop_aws_iaas.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
